838ee80404d21bf12e9517339aaa7474c9e707b1
Aysha Afa <aysha.afa@gmail.com>
2025-02-10 17:01:08 +0530

    tax calculation,grand total,and add to cart function has been added

--- shoppingCart.js
+++ shoppingCart.js
@@ -19,3 +19,43 @@ async function getAllProducts() {
     return null;
   }
 }
+
+async function taxCalculation(totalPriceWithoutTax) {
+  return totalPriceWithoutTax * taxPercent;
+
+}
+
+async function grandTotal(totalPriceWithoutTax) {
+  let taxAmount = await taxCalculation(totalPriceWithoutTax);
+  return Math.round((taxAmount + totalPriceWithoutTax) * 100) / 100;
+}
+
+async function addToCart(cart) {
+  try {
+    let subTotal=0;
+    let taxAmount=0;
+    let totalAmount=0;
+    let allProducts = await getAllProducts();
+for(let item of cart){
+    let Found = allProducts.find(
+        (Products) => Products.title === item.products
+      );
+      let total = Found.price * item.quantities;
+      let payableAmount = await grandTotal(total);
+      subTotal=subTotal+total;
+      let taxableAmount=await taxCalculation(total);
+      taxAmount=taxAmount+taxableAmount;
+      totalAmount=Math.round((totalAmount+payableAmount)*100)/100;
+}
+    let result={
+        amountWithoutTax:Math.round((subTotal)*100)/100,
+        taxCharges:Math.round((taxAmount)*100)/100,
+        payableCharge:totalAmount
+    };
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
